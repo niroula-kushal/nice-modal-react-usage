4346e636e495579986598f3b9cc3e1268c3e1a6c
Kushal Niroula <niroulaKushal31@gmail.com>
2026-05-06 19:58:04 +0545

    Pass props through NiceModal.show for side modal

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import NiceModal from "@ebay/nice-modal-react"
+import App from "./App"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NiceModal.Provider>
+      <App />
+    </NiceModal.Provider>
+  </React.StrictMode>,
+)
